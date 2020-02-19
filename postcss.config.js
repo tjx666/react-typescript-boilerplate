a179d6561ea7dc4e99700c3d1e8acc9f8091583e
YuTengjing <ytj2713151713@gmail.com>
2020-02-19 17:11:42 +0800

    build: add postcss config

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable global-require, @typescript-eslint/no-var-requires */
+
+module.exports = {
+    plugins: [
+        // 修复一些 flex 的 bug
+        require('postcss-flexbugs-fixes'),
+        // 支持一些现代浏览器 CSS 特性，支持 browserslist
+        require('postcss-preset-env')({
+            // 自动添加浏览器头
+            autoprefixer: {
+                // will add prefixes only for final and IE versions of specification
+                flexbox: 'no-2009',
+            },
+            stage: 3,
+        }),
+        // 根据 browserslist 自动导入部分 normalize.css 的内容
+        require('postcss-normalize'),
+    ],
+};
