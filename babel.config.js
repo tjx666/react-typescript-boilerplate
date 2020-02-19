cb87873f9f819a53e7f8659c94ecdaacecbf2b56
YuTengjing <ytj2713151713@gmail.com>
2020-02-19 17:10:38 +0800

    build(babel): add babel config

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+const envPreset = [
+    '@babel/preset-env',
+    {
+        // 只导入需要的 polyfill
+        useBuiltIns: 'usage',
+        // 指定 corjs 版本
+        corejs: 3,
+        // 禁用模块化方案转换
+        modules: false,
+    },
+];
+
+module.exports = function(api) {
+    api.cache(true);
+    return {
+        presets: ['@babel/preset-typescript', envPreset],
+        plugins: ['@babel/plugin-transform-runtime'],
+        env: {
+            development: {
+                presets: [['@babel/preset-react', { development: true }]],
+                plugins: ['react-hot-loader/babel'],
+            },
+            production: {
+                presets: ['@babel/preset-react'],
+                plugins: [
+                    'babel-plugin-dev-expression',
+                    '@babel/plugin-transform-react-constant-elements',
+                    '@babel/plugin-transform-react-inline-elements',
+                ],
+            },
+        },
+    };
+};
