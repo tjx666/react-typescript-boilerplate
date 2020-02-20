55562889a2e45fce67dd421b9e35f92a8030d41c
YuTengjing <ytj2713151713@gmail.com>
2020-02-20 20:07:21 +0800

    chore(types): add type definition for image module

---
+++ src/typings/index.d.ts
@@ -0,0 +1,33 @@
+declare module '*.svg' {
+    import * as React from 'react';
+
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
+    const src: string;
+    export default src;
+}
+
+declare module '*.bmp' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.gif' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.jpg' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.jpeg' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.png' {
+    const path: string;
+    export default path;
+}
