3c5a6dfaaf37e3531fe507e117da96b9cc3b92b2
YuTengjing <ytj2713151713@gmail.com>
2020-02-23 19:07:19 +0800

    refactor: exact setupMiddlewares to index of middlewares fold

---
+++ scripts/middlewares/index.ts
@@ -0,0 +1,23 @@
+import { Compiler } from 'webpack';
+import { Express } from 'express';
+import historyFallback from 'connect-history-api-fallback';
+import cors from 'cors';
+import proxyMiddleware from './proxyMiddleware';
+import webpackMiddleware from './webpackMiddleware';
+
+/**
+ * 配置中间件
+ */
+export default function setupMiddlewares(server: Express, compiler: Compiler) {
+    // 设置代理
+    proxyMiddleware(server);
+
+    // 使用 browserRouter 时，需要重定向所有 html 页面到首页
+    server.use(historyFallback());
+
+    // 开发 chrome 扩展的时候可能需要开启跨域，参考：https://juejin.im/post/5e2027096fb9a02fe971f6b8
+    server.use(cors());
+
+    // webpack 相关中间件
+    server.use(webpackMiddleware(compiler));
+}
--- scripts/start.ts
+++ scripts/start.ts
@@ -1,35 +1,13 @@
 import chalk from 'chalk';
 import logSymbols from 'log-symbols';
-import webpack, { Compiler } from 'webpack';
-import express, { Express } from 'express';
-
-// 中间件
-import historyFallback from 'connect-history-api-fallback';
-import cors from 'cors';
-import proxyMiddleware from './middlewares/proxyMiddleware';
-import webpackMiddleware from './middlewares/webpackMiddleware';
+import webpack from 'webpack';
+import express from 'express';
 
 import devConfig from './configs/webpack.dev';
 import { HOST, DEFAULT_PORT } from './utils/constants';
 import getPort from './utils/getPort';
+import setupMiddlewares from './middlewares';
 import openBrowser from './utils/openBrowser';
-
-/**
- * 配置中间件
- */
-function setupMiddlewares(compiler: Compiler, server: Express) {
-    // 设置代理
-    proxyMiddleware(server);
-
-    // 使用 browserRouter 时，需要重定向所有 html 页面到首页
-    server.use(historyFallback());
-
-    // 开发 chrome 扩展的时候可能需要开启跨域，参考：https://juejin.im/post/5e2027096fb9a02fe971f6b8
-    server.use(cors());
-
-    // webpack 相关中间件
-    server.use(webpackMiddleware(compiler));
-}
 
 async function start() {
     const PORT = await getPort(HOST, DEFAULT_PORT);
@@ -39,7 +17,7 @@ async function start() {
     const compiler = webpack(devConfig);
 
     const devServer = express();
-    setupMiddlewares(compiler, devServer);
+    setupMiddlewares(devServer, compiler);
     openBrowser(compiler, address);
 
     const httpServer = devServer.listen(PORT, HOST, err => {
